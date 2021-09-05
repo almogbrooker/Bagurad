3b5c41ab18bbb4b0025e07dbdcbfe5fc735e43f4
almog brooker <almogg1000@gmail.com>
2021-09-05 15:32:49 +0300

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/almog/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/almog/Android/baguard_full" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/almog/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/almog/Android/baguard_full"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/almog/Android/baguard_full/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/almog/Android/baguard_full/lib/main.dart"
+)
