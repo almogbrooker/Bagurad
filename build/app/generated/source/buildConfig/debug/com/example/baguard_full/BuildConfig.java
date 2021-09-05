2b735d76d74ff609f7906ecc230001468edff369
almogbrooker <almogg1000@gmail.com>
2021-09-05 15:42:15 +0300

    ff

---
+++ build/app/generated/source/buildConfig/debug/com/example/baguard_full/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.baguard_full;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.baguard_full";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
